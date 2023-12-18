0f82d2da94957c4d9120236de60e1fd590e423b5
Sam-FED <untiverossamantha@hotmail.com>
2023-12-18 10:12:41 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+// alert("Hello World");
+// affiche texte dans petite fenetre pop up
+
+// console.log("Hello World");
+// affiche hello world dans la console
+// ici on affiche du texte mais peut etre une variable, un calcul,...
+// on met les guillemets pour du texte
+// pour écrire un ' il faut mettre \' pour que l'apostrophe s'affiche
+
+// console.log( 500 +100);
+// calculs -> le resultat va s'afficher sur la console
+//  ++ = -1    et -- = -1
+
+// console.log("Hello"+ "World");
+// concatenation: combiner plusieurs chaines de caractere
+
+// console.log(2+2==4);
+// booleans valeurs TRUE or FALSE. == veut direr strictement égal
+// = pour la déclaration de variable
+
+// let monPrenom = "Sam"
+
+// const monPrenom = "Sam" 
+// une constante ne changera jamais
+
+// let monAge = "32"
+// let pour déclarer une variable qui va décrire ce qu'on veut faire
+// convention de nommage premier mot en minuscule et les nouveau avec premiere lettre en majuscule ex: monPremnom
+
+// console.log("Mon prénom est " + monPrenom + " et j'ai " + monAge + " ans.");
+// pour aller chercher ma variable je ne mets pas de "" car sinon ça va apparaitre comme du texte monPrenom et non pas Sam
+
+// console.log(`Mon prénom est ${monPrenom} et j'ai ${monAge} ans.`);
+// monAge = 33
+// console.log(`Mon prénom est ${monPrenom} et j'ai ${monAge} ans.`);
+// Avec le backtick on peut concaténer de cette façon
+// let permet le changement de variable
+
+
+let monPrenom ="Sam"
+    monNom = "Untiveros"
+// On peut déclarer plusoieurs variables de cette manière
+
+// Les variables
+// Une variable ne peut pas contenir de nombres, espaces, caracteres spéciaux, mots utilisés par javascript
+/*le scope (la portée) d'une variable peut etre soit globale soit locale 
+/*
+
+
+
